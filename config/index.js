806dc562b6c58c31f1077dcdeaf174a9621de11b
= <=>
2018-03-23 16:10:29 -0400

    Change db location to mlab
    So devs don't need to have a local mongodb instance,
    they can use this mlab one

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // MongoDB
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/smartcampus',
+  // MongoDB (use mlab test database by default)
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://root:root@ds115219.mlab.com:15219/smartcampus',
 
   // JWT - Use environment variable in production!
   JWT_SECRET: process.env.JWT_SECRET || 'super secret',
