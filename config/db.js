6dfc028248c8790045a38f04238af74b97355d8e
Zach Shaver <shaverz@uwindsor.ca>
2018-09-17 23:57:58 -0400

    Fix dev db config and enable logging

--- config/db.js
+++ config/db.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_USERNAME || null,
+    password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME || "smartcampus_dev",
     host: process.env.DB_HOSTNAME || "127.0.0.1",
     dialect: 'mysql',
     operatorsAliases: "Sequelize.Op",
-    logging: false
   },
   test: {
     username: process.env.DB_USERNAME || "root",
